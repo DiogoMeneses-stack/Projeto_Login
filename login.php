5947140243d24eac1f256bbadef8c972901ddc5c
= <henriquediogo434@gmail.com>
2024-06-06 11:46:14 -0300

    Initial Commit

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Login</title>
+</head>
+<body class="d-flex align-items-center py-4 bg-body-tertiary">
+    <main class="w-100 m-auto">
+        <form action="">
+            <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" class="m-4" height="57" width="72" />
+            <h1 class="h3 mb-3 fw-normal">Digite o Login</h1>
+            <div class="form-floating">
+                <input type="email" class="form-control" id="floatingInput" placeholder="seu email..."/>
+            </div>
+        </form>
+    </main>
+</body>
+</html>
